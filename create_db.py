90551198187b5857493db25b3fa0a38f558e93a8
Jihad <Jihad.alaswa@d-n.be>
2025-05-07 23:40:20 +0200

    update file

--- create_db.py
+++ create_db.py
@@ -4,16 +4,16 @@ import subprocess
 def run_alembic():
     print("Running Alembic revision and upgrade...")
 
+    # Upgrade database
+    subprocess.run([
+        "alembic", "upgrade", "head"
+    ], check=True)
 
     # Generate revision
     subprocess.run([
         "alembic", "revision", "--autogenerate", "-m", "Auto migration"
     ], check=True)
-
-    # Apply migrations
-    subprocess.run([
-        "alembic", "upgrade", "head"
-    ], check=True)
+  
 
 if __name__ == "__main__":
     run_alembic()
