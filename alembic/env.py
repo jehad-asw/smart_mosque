9ccab3006ce0a0efd4e6446caadabdefe58a60c2
Jihad <Jihad.alaswa@d-n.be>
2025-05-07 23:28:46 +0200

    fix create db

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from app.config.database import Base
-from app.models import user, teacher, student
+from app.models import *
 
 from alembic import context
 from dotenv import load_dotenv
